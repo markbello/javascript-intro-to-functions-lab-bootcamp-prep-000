a9e7e88fb611ddbecaed7eee8e6708e621bb74a1
Mark Bello <mcbello51286@gmail.com>
2017-11-15 18:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase() === "i love you, grandma."){
     return "I love you, too.";
-  } else if(string != string.toLowerCase()){
+  } else if(string.toLowerCase === string){
     return "I can't hear you!";
   } else if(string.toUpperCase === string){
     return "YES INDEED!";
